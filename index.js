49d78950ea17e95d0f4b99a0c25c1f33f46777db
Jérémy Giral <jeremy.giral@BIAL-X.local>
2018-01-24 15:23:05 +0100

    second commitfds s

--- index.js
+++ index.js
@@ -15,105 +15,140 @@ restService.use(bodyParser.json());
 
 restService.post("/echo", function(req, res) {
   var speech =
-    req.body.result &&
-    req.body.result.parameters &&
-    req.body.result.parameters.parametre
-      ? req.body.result.parameters.parametre+' super non ?!'
-      : "On dirait qu'il y a un problème, essaye encore.";
+  req.body.result &&
+  req.body.result.parameters &&
+  req.body.result.parameters.parametre
+  ? req.body.result.parameters.parametre+' super non ?!'
+  : "On dirait qu'il y a un problème, essaye encore.";
   //var followup="{\"followupEvent\": {\"name\": \"Commencement\",\"data\": {\"test\":\"ceci est un test\"}}}";
-
+  var followintent={name: "Commencement",data:{test:"test"}};
+  // return res.json({
+  // speech: speech,
+  // displayText: speech,
+  // source: "webhook-echo-sample"
+  // });
+  // });
+  //
+  // "fulfillment": {
+  // "speech": "Oui super non ?!",
+  // "source": "webhook-echo-sample",
+  // "displayText": "Oui super non ?!",
+  // "messages": [
+  // {
+  // "type": 0,
+  // "speech": "Oui super non ?!"
+  // }
+  // ]
+  // }
+  // {
+  // "fulfillmentText": string,
+  // "fulfillmentMessages": [
+  // //peut etre card quick replies... a tester et en fonction des platforms
+  // {"text": [string]}
+  // ],
+  // "followupEventInput": {
+  // "name": string,
+  // "parameters": {
+  // object
+  // },
+  // "languageCode": string,
+  // }
+  // }
   return res.json({
-    speech: speech,
-    displayText: speech,
-    source: "webhook-echo-sample"
+    fulfillmentText: speech,
+    fulfillmentMessages: [{text: [speech]}],
+    followupEventInput:followintent
   });
 });
+
+
+
 
 restService.post("/audio", function(req, res) {
   var speech = "";
   switch (req.body.result.parameters.AudioSample.toLowerCase()) {
     //Speech Synthesis Markup Language
     case "music one":
-      speech =
-        '<speak><audio src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
-      break;
+    speech =
+    '<speak><audio src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
+    break;
     case "music two":
-      speech =
-        '<speak><audio clipBegin="1s" clipEnd="3s" src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
-      break;
+    speech =
+    '<speak><audio clipBegin="1s" clipEnd="3s" src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
+    break;
     case "music three":
-      speech =
-        '<speak><audio repeatCount="2" soundLevel="-15db" src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
-      break;
+    speech =
+    '<speak><audio repeatCount="2" soundLevel="-15db" src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
+    break;
     case "music four":
-      speech =
-        '<speak><audio speed="200%" src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
-      break;
+    speech =
+    '<speak><audio speed="200%" src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
+    break;
     case "music five":
-      speech =
-        '<audio src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio>';
-      break;
+    speech =
+    '<audio src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio>';
+    break;
     case "delay":
-      speech =
-        '<speak>Let me take a break for 3 seconds. <break time="3s"/> I am back again.</speak>';
-      break;
+    speech =
+    '<speak>Let me take a break for 3 seconds. <break time="3s"/> I am back again.</speak>';
+    break;
     //https://www.w3.org/TR/speech-synthesis/#S3.2.3
     case "cardinal":
-      speech = '<speak><say-as interpret-as="cardinal">12345</say-as></speak>';
-      break;
+    speech = '<speak><say-as interpret-as="cardinal">12345</say-as></speak>';
+    break;
     case "ordinal":
-      speech =
-        '<speak>I stood <say-as interpret-as="ordinal">10</say-as> in the class exams.</speak>';
-      break;
+    speech =
+    '<speak>I stood <say-as interpret-as="ordinal">10</say-as> in the class exams.</speak>';
+    break;
     case "characters":
-      speech =
-        '<speak>Hello is spelled as <say-as interpret-as="characters">Hello</say-as></speak>';
-      break;
+    speech =
+    '<speak>Hello is spelled as <say-as interpret-as="characters">Hello</say-as></speak>';
+    break;
     case "fraction":
-      speech =
-        '<speak>Rather than saying 24+3/4, I should say <say-as interpret-as="fraction">24+3/4</say-as></speak>';
-      break;
+    speech =
+    '<speak>Rather than saying 24+3/4, I should say <say-as interpret-as="fraction">24+3/4</say-as></speak>';
+    break;
     case "bleep":
-      speech =
-        '<speak>I do not want to say <say-as interpret-as="bleep">F&%$#</say-as> word</speak>';
-      break;
+    speech =
+    '<speak>I do not want to say <say-as interpret-as="bleep">F&%$#</say-as> word</speak>';
+    break;
     case "unit":
-      speech =
-        '<speak>This road is <say-as interpret-as="unit">50 foot</say-as> wide</speak>';
-      break;
+    speech =
+    '<speak>This road is <say-as interpret-as="unit">50 foot</say-as> wide</speak>';
+    break;
     case "verbatim":
-      speech =
-        '<speak>You spell HELLO as <say-as interpret-as="verbatim">hello</say-as></speak>';
-      break;
+    speech =
+    '<speak>You spell HELLO as <say-as interpret-as="verbatim">hello</say-as></speak>';
+    break;
     case "date one":
-      speech =
-        '<speak>Today is <say-as interpret-as="date" format="yyyymmdd" detail="1">2017-12-16</say-as></speak>';
-      break;
+    speech =
+    '<speak>Today is <say-as interpret-as="date" format="yyyymmdd" detail="1">2017-12-16</say-as></speak>';
+    break;
     case "date two":
-      speech =
-        '<speak>Today is <say-as interpret-as="date" format="dm" detail="1">16-12</say-as></speak>';
-      break;
+    speech =
+    '<speak>Today is <say-as interpret-as="date" format="dm" detail="1">16-12</say-as></speak>';
+    break;
     case "date three":
-      speech =
-        '<speak>Today is <say-as interpret-as="date" format="dmy" detail="1">16-12-2017</say-as></speak>';
-      break;
+    speech =
+    '<speak>Today is <say-as interpret-as="date" format="dmy" detail="1">16-12-2017</say-as></speak>';
+    break;
     case "time":
-      speech =
-        '<speak>It is <say-as interpret-as="time" format="hms12">2:30pm</say-as> now</speak>';
-      break;
+    speech =
+    '<speak>It is <say-as interpret-as="time" format="hms12">2:30pm</say-as> now</speak>';
+    break;
     case "telephone one":
-      speech =
-        '<speak><say-as interpret-as="telephone" format="91">09012345678</say-as> </speak>';
-      break;
+    speech =
+    '<speak><say-as interpret-as="telephone" format="91">09012345678</say-as> </speak>';
+    break;
     case "telephone two":
-      speech =
-        '<speak><say-as interpret-as="telephone" format="1">(781) 771-7777</say-as> </speak>';
-      break;
+    speech =
+    '<speak><say-as interpret-as="telephone" format="1">(781) 771-7777</say-as> </speak>';
+    break;
     // https://www.w3.org/TR/2005/NOTE-ssml-sayas-20050526/#S3.3
     case "alternate":
-      speech =
-        '<speak>IPL stands for <sub alias="indian premier league">IPL</sub></speak>';
-      break;
+    speech =
+    '<speak>IPL stands for <sub alias="indian premier league">IPL</sub></speak>';
+    break;
   }
   return res.json({
     speech: speech,
@@ -125,9 +160,9 @@ restService.post("/audio", function(req, res) {
 restService.post("/video", function(req, res) {
   return res.json({
     speech:
-      '<speak>  <audio src="https://www.youtube.com/watch?v=VX7SSnvpj-8">did not get your MP3 audio file</audio></speak>',
+    '<speak>  <audio src="https://www.youtube.com/watch?v=VX7SSnvpj-8">did not get your MP3 audio file</audio></speak>',
     displayText:
-      '<speak>  <audio src="https://www.youtube.com/watch?v=VX7SSnvpj-8">did not get your MP3 audio file</audio></speak>',
+    '<speak>  <audio src="https://www.youtube.com/watch?v=VX7SSnvpj-8">did not get your MP3 audio file</audio></speak>',
     source: "webhook-echo-sample"
   });
 });
@@ -155,7 +190,7 @@ restService.post("/slack-test", function(req, res) {
         ],
 
         thumb_url:
-          "https://stiltsoft.com/blog/wp-content/uploads/2016/01/5.jira_.png"
+        "https://stiltsoft.com/blog/wp-content/uploads/2016/01/5.jira_.png"
       },
       {
         title: "Story status count",
