a84d99fbdc1db331fcd24e18b78730c1ef46aedd
Jérémy Giral <jeremy.giral@BIAL-X.local>
2018-01-24 14:25:42 +0100

    second commit

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ restService.post("/echo", function(req, res) {
   var speech =
     req.body.result &&
     req.body.result.parameters &&
-    req.body.result.parameters.echoText
-      ? req.body.result.parameters.echoText
-      : "Seems like some problem. Speak again.";
+    req.body.result.parameters.parametre
+      ? req.body.result.parameters.parametre
+      : "On dirait qu'il y a un problème, essaye encore.";
   return res.json({
     speech: speech,
     displayText: speech,
@@ -30,7 +30,7 @@ restService.post("/echo", function(req, res) {
 restService.post("/audio", function(req, res) {
   var speech = "";
   switch (req.body.result.parameters.AudioSample.toLowerCase()) {
-    //Speech Synthesis Markup Language 
+    //Speech Synthesis Markup Language
     case "music one":
       speech =
         '<speak><audio src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
